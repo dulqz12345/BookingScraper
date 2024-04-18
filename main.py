fa6bca08b76c5e8355107e6cecb691d3017c4f00
Dulqz <you@example.com>
2024-04-18 17:50:22 +0200

    Web scraping from website - getting response

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = ('https://www.booking.com/searchresults.pl.html?ss=Gda%C5%84sk%2C+Polska&efdco=1&label=gen173nr'
-       '-1FCAEoggI46AdIHlgEaLYBiAEBmAEeuAEXyAEM2AEB6AEB'
-       '-AECiAIBqAIDuAK184SxBsACAdICJGM4MTZmMzk0LTYzZDMtNDQ2ZS05ZTI2LTYzYzUwN2E1NmZhZtgCBeACAQ&aid=304142&lang=pl&sb'
-       '=1&src_elem=sb&src=index&dest_id=-501400&dest_type=city&checkin=2024-07-13&checkout=2024-07-21&group_adults=2'
-       '&no_rooms=1&group_children=0')
 
-response = requests.get(URL)
-print(response.status_code)
-print(response.url)
-booking_gdansk = response.text
+def scrape_booking(search_query):
+    URL = f"https://www.booking.com/search.html?ss={search_query}"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/123.0.0.0 Safari/537.36'}
+    response = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(booking_gdansk, "html.parser")
-#print(soup.prettify())
-print(soup.title)
+    if response.status_code == 200:
+        print(response.url)
+
+        booking_gdansk = response.text
+        soup = BeautifulSoup(booking_gdansk, "html.parser")
+        # print(soup.prettify())
+        print(soup.title)
+        hotel_names = [name.text.strip() for name in soup.find_all('span', class_='sr-hotel__name')]
+        return hotel_names
+    else:
+        print("Failed to fetch page")
+        return []
+
+
+search_query = "Gdansk"  # Example search query
+hotels_found = scrape_booking(search_query)
+print(hotels_found)
 
 # def inc(x):
 #     return x + 1
